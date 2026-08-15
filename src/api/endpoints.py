bf5915f2acaf4181c8578c1fd44838e26b360712
T1Ahri <github@nathangaubert.fr>
2026-08-15 21:22:52 +0200

    More endpoints added

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -8,19 +8,45 @@ class Region:
     EUROPE = "europe"
     SEAS = "sea"
 
-#class Platform:
+class Platform:
+    EUW1 = "euw1"
     
 
 
 # --- ACCOUNT-V1 ---
 
+def account_by_puuid_url(region: Region, puuid: str) -> dict:
+    return f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-puuid/{puuid}"
+
 def account_by_riotId_url(region: Region, game_name: str, tag_line: str) -> str:
     return f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
 
 
+# --- CHAMPION-MASTERY-V4 ---
+
+def all_champ_mastery_entries_by_puuid_url(region: Platform, puuid: str) -> list:
+    return f"https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{puuid}"
+
+def champ_mastery_entrie_by_puuid_champId_url(region: Platform, puuid: str, champ_id: int) -> dict:
+    return f"https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{puuid}/by-champion/{champ_id}"
+
+def top_champ_mastery_entries_params(count: int | None = None) -> dict:
+    params = {
+        'count': count
+    }
+    return params
+
+def top_champ_mastery_entries_by_puuid_url(region: Platform, puuid: str) -> list:
+    return f"https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{puuid}/top"
+
+def total_champ_mastery_score_by_puuid(region: Platform, puuid: str) -> int:
+    return f"https://{region}.api.riotgames.com/lol/champion-mastery/v4/scores/by-puuid/{puuid}"
+
+
 # --- SUMMONER-V4 ---
 
-#def summoner_by_puuid_url(region)
+def summoner_by_puuid_url(region: Platform, puuid: str) -> dict:
+    return f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}"
 
 
 # --- MATCH-V5 ---
@@ -36,7 +62,7 @@ def matchs_ids_params(start_time: int | None = None, end_time: int | None = None
     }
     return {key: value for key, value in params.items() if value is not None}
 
-def match_ids_by_puuid_url(region: str, puuid: str, ) -> str:
+def match_ids_by_puuid_url(region: str, puuid: str) -> str:
     return f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids"
 
 def match_detail_by_matchId_url(region: str, match_id: int) -> str:
